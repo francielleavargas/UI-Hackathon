4dc068b16357f4451194b1baeea66a4bb488fed9
Francielle A. Vargas <franciellealvargas@gmail.com>
2020-06-11 19:22:50 -0300

    Add files via upload

--- antisemitism_data_science.py
+++ antisemitism_data_science.py
@@ -7,14 +7,57 @@
 # Total entries classified as antisemitic: 436
 # Total entries classified as not antisemitic: 569
 
+
+from collections import Counter
+import matplotlib.pyplot as plt
+from nltk.stem import WordNetLemmatizer 
 import numpy as np
 import pandas as pd
-import json
+import string
+from textblob import TextBlob
+import langid
+import emoji
 import spacy
+import json
+import nltk
 import re
-from collections import Counter
-nlp = spacy.load("en_core_web_sm")
 
+
+#Variable declarations-------------------------------------------------------
+nlp = spacy.load("en_core_web_sm")
+nlp_1 = spacy.load("en_core_web_md")
+
+#Functions-------------------------------------------------------------------
+def hash_fix(h):
+	h1 = re.sub(r'[0-9]+', '', h)
+	h2 = re.sub(r'#', '', h1)
+	h3 = segment(str(h2))
+	h4 = ' '.join(map(str, h3)) 
+	return h4
+
+def remove_punct(text):
+	text  = "".join([char for char in text if char not in string.punctuation])
+	text = re.sub('[0-9]+', '', text)
+	return text
+
+def give_emoji_free_text(text):
+	allchars = [str for str in text]
+	emoji_list = [c for c in allchars if c in emoji.UNICODE_EMOJI]
+	clean_text = ' '.join([str for str in text.split() if not any(i in str for i in emoji_list)])
+	return clean_text
+
+def extract_emojis(str):
+  return ''.join(c for c in str if c in emoji.UNICODE_EMOJI)
+
+def remove_repetidos(lista):
+	l = []
+	for i in lista:
+		if i not in l:
+			l.append(i)
+	l.sort()
+	return l
+
+#Create a dataframe-------------------------------------------------------
 # Open json file from same folder, read in data, and convert to a dictionary
 json_dict = {}
 with open('hackathon2.json') as file:
@@ -43,7 +86,7 @@ tweets_df['is_about_the_holocaust'] = np.array([tweet['is_about_the_holocaust'] 
 tweets_df['ihra_section'] = np.array([tweet['ihra_section'] for tweet in json_dict])
 
 
-#Classes balancing 
+#Classes balancing----------------------------------------------------------- 
 classe_1  = tweets_df[tweets_df.antisemitism_rating == '1'].head(285)
 classe_2 = tweets_df[tweets_df.antisemitism_rating == '2']
 classe_3 = tweets_df[tweets_df.antisemitism_rating == '3']
@@ -51,46 +94,80 @@ classe_4 = tweets_df[tweets_df.antisemitism_rating == '4']
 classe_5 = tweets_df[tweets_df.antisemitism_rating == '5']
 
 #Slip in binary class
-class_A = pd.concat([classe_1, classe_2, classe_3])
-class_N = pd.concat([classe_4, classe_5])
+c0 = pd.concat([classe_1, classe_2, classe_3])
+c1 = pd.concat([classe_4, classe_5])
+c3 = pd.concat([c0, c1])
 
-#Variable declarations
-verb = []
-noun = []
-propn = []
-adp = []
-sym = []
-aux = []
-adj = []
-punct = []
-conj = []
-pron = []
-propn = []
-adve = []
-num = []
-ax1 = []
-ax2 = []
-ax3 = []
-ax4 = []
+data = []
+ner =[]
+#Starting the dataset exploration and annotation------------------------------
+for ad, texts in enumerate(c3['tweet']):
+	
+	#Data cleaning
+	texts = texts.lower() 
+	texts = texts.lstrip()
+	texts = texts.replace(r"(http|@)\S+", "")
+	texts = texts.replace(r"::", ": :")
+	texts = texts.replace(r"’", "")
+	texts = texts.replace(r"’", "")
+	texts = texts.replace(r"|", "")
+	texts = texts.replace(r"/", "")
+	texts = texts.replace(r"", "")
+	texts = texts.replace(r"'", "")
+	texts = texts.replace(r"*", "")
+	texts = texts.replace(r"!", "")
+	texts = texts.replace(r"?", "")
+	texts = texts.replace(r"«", "")
+	texts = texts.replace(r"»", "")
+	texts = texts.replace(r"(", "")
+	texts = texts.replace(r"‘", "")
+	texts = texts.replace(r"“", "")
+	texts = texts.replace(r"”", "")
+	texts = texts.replace(r"“", "")
+	texts = texts.replace(r";", "")
+	texts = texts.replace(r",", "")
+	texts = texts.replace(r".", "")
+	texts = texts.replace(r'"', "")
+	texts = texts.replace(r':', "")
+	texts = texts.replace(r'_', "")
+	texts = texts.replace(r'&', "")
+	texts = texts.replace(r")", "")
+	texts = texts.replace(r'¿', "")
+	texts = texts.replace(r"[^a-z\':_]", " ")
+	texts = texts.replace(r"(can't|cannot)", 'can not')
+	texts = texts.replace(r"n't", ' not')
+	texts = re.sub('@[^\s]+','',texts) #remove usernames
+	texts = give_emoji_free_text(texts)
+	texts = ' '.join(re.sub("(@[A-Za-z0-9]+)|(#[A-Za-z0-9]+)", " ", texts).split())
+	texts = re.sub(r'http\S+', '', texts)
+		#texts = re.sub('[0-9]+', '', texts) #remove pontuaction
+	#texts = re.sub(r'''(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))''', " ", texts)
+	#c1['hashtag'] = c1['tweet'].apply(lambda x: re.findall(r'\B#\w*[a-zA-Z]+\w*', texts)) #creating a new column
+	#texts = re.findall(r"#(\w+)", texts)
+	
+	doc = nlp(texts)
+	#print(ad, doc)
 
-#Starting the dataset exploration and annotation
-for ad, item in enumerate(class_A['tweet']):
-	result = re.sub(r"http\S+", "", item)
-	doc = nlp(result)
+	#Dependece Tree
+	#for token in doc:
+		#print(token.text, ';', token.dep_, ';', token.head.text, ';', token.head.pos_,)
+
+	#POS tagging
+	#for token in doc:
+		#if token.tag == 'NOUN' or token.tag == 'VERB' or token.tag == 'ADJ' or token.tag == 'ADV': 
+		#print(token.text, ';', token.lemma_, ';', token.pos_, ';', token.tag_, ';', token.dep_, ';', token.shape_, ';', token.is_alpha, ';', token.is_stop)
 	
 	#NER annotation
 	for ent in doc.ents:
-		print(ent.text, ';', ent.start_char, ';', ent.end_char, ';', ent.label_)
-		if ent.label_ == 'PERSON':
-			ax1.append(ent.text)
-		if ent.label_ == 'LOC':
-			ax2.append(ent.text)
-		if ent.label_ == 'ORG':
-			ax3.append(ent.text)
-		if ent.label_ == 'MISC':
-			ax4.append(ent.text)
+		if (ent.label_ == 'EVENT') or (ent.label_ == 'FAC') or (ent.label_ == 'GPE') or (ent.label_ == 'LOC') or (ent.label_ == 'NORP') or (ent.label_ == 'ORG') or (ent.label_ == 'PERSON'):
+			#print(ad, ent.text, ent.label_)
+			lang = langid.classify(ent.text)
+			if 'en' in lang:
+				lemmatizer = WordNetLemmatizer()
+				ner.append(lemmatizer.lemmatize(ent.text))
 
-print(len(ax1))
-print(len(ax2))
-print(len(ax3))
-print(len(ax4))
+	ner = remove_repetidos(ner)
+	lista = [0 for j in range(len(ner))]
+	
+
+					
