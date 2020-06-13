5c6c96fd1bfd08d97871f40d1c97968ea227a3cc
Francielle A. Vargas <franciellealvargas@gmail.com>
2020-06-13 20:11:23 -0300

    Add files via upload

--- AM_antiSemitism.py
+++ AM_antiSemitism.py
@@ -3,10 +3,8 @@
 
 import rdflib
 import string
-import pandas
+import pandas as pd
 import random
-#from pandas.plotting import scatter_matrixsudo
-from pandas.tools.plotting import scatter_matrix
 import matplotlib.pyplot as plt
 from sklearn import model_selection
 from sklearn.metrics import classification_report
@@ -21,32 +19,22 @@ from sklearn.svm import SVC
 
 
 # Load dataset
-url = "tw_pol.txt"
-names = ['Político', 'Não-Político'] #, 'petal-length', 'petal-width', 'class']
-dataset = pandas.read_csv(url, names=names)
+dataset = pd.read_csv("features.csv")
 
-#Statistic information dataset
+#Apresenta informações do dataset 
+#dataset.info()
+#print(dataset.head())
 #print(dataset.shape)
 #print(dataset.describe())
-#print(dataset.groupby('class').size())
-# box and whisker plots
-#dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
-#plt.show()
 
 
-# Plot the statistic of dataset
-#UNIVARIABLE
-#dataset.hist()
-#plt.show()
+#Separando as features e a class
+X = dataset.iloc[:, :-1]
+Y = dataset.iloc[:,-1]
+#print(X)
+#print(Y)
 
-#*******MULTIVARIABLE*********
-#scatter_matrix(dataset)
-#plt.show()
-
-array = dataset.values
-X = array[:,0:1]
-Y = array[:,1]
-validation_size = 0.20
+validation_size = 0.10
 seed = 7
 X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
 scoring = 'accuracy'
@@ -62,17 +50,30 @@ models.append(('SVM', SVC()))
 results = []
 names = []
 for name, model in models:
-	kfold = model_selection.KFold(n_splits=10, random_state=seed)
+	kfold = model_selection.KFold(n_splits=10, random_state=None)
 	cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
 	results.append(cv_results)
 	names.append(name)
 	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
 	print(msg)
 
-#---------------------------------------------------------------------------------------------------------------------------------------
-knn = KNeighborsClassifier()
-knn.fit(X_train, Y_train)
-predictions = knn.predict(X_validation)
+
+
+#SVMmodel = SVC()
+#SVMmodel.fit(X_train, Y_train)
+#predictions = SVMmodel.predict(X_validation)
 #print(accuracy_score(Y_validation, predictions))
 #print(confusion_matrix(Y_validation, predictions))
 #print(classification_report(Y_validation, predictions))
+
+
+#KNNmodel = KNeighborsClassifier()
+#KNNmodel.fit(X_train, Y_train)
+#predictions = KNNmodel.predict(X_validation)
+#print(accuracy_score(Y_validation, predictions))
+#print(confusion_matrix(Y_validation, predictions))
+#print(classification_report(Y_validation, predictions))
+
+
+
+
